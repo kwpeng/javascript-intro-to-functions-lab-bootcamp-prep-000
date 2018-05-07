f9b705550379f86120977a4e6a347e2526918719
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-05-07 06:50:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return ('I can\'t hear you!');
   }
   else if (string.toUpperCase() === string) {
-    console.log('YES INDEED!');
+    return('YES INDEED!');
     }
   else {
     console.log('I love you, too.');
