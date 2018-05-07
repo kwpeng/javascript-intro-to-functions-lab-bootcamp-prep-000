8dead0e767be6cb52613ec2e85aa2cf84dbdfb80
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-05-07 06:49:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function whisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    console.log('I can\'t hear you!');
+    return ('I can\'t hear you!');
   }
   else if (string.toUpperCase() === string) {
     console.log('YES INDEED!');
