ad5cb7406f6889539ddabeb35ba23267aa4a0d7c
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-05-07 06:46:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     console.log("I can't hear you!");
   }
-  if (string.toUpperCase() === string) {
+  else if (string.toUpperCase() === string) {
     console.log("YES INDEED!");
     }
   else {
